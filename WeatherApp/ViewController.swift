a3b9e2d5712d083788f83a8a6df2124ee0dcae62
Rent Advisor <rentadvisor@Rents-MacBook-Pro.local>
2018-01-18 12:14:33 -0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Rent Advisor on 18/01/18.
+//  Copyright © 2018 nvjuanddl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
